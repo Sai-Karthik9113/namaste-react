e4da20c32cbf7021f1c596ecd0f20cab80e7c59d
Sai-Karthik9113 <sai9898karthik@gmail.com>
2025-02-07 16:10:24 +0530

    Ignite the app with parcel

--- App.js
+++ App.js
@@ -9,15 +9,21 @@
 // root.render(header);
 
 // Example 02: Complex React Structure
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm a h1 tag of first child"),
-    React.createElement("h2", {}, "I'm a h2 tag of first child"),
+    React.createElement("h1", {}, "List out the h tags"),
+    React.createElement("h2", {}, "h1, h2, h3, h4, h5, h6"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm a h1 tag of second child"),
-    React.createElement("h2", {}, "I'm a h2 tag of second child"),
+    React.createElement(
+      "h1",
+      {},
+      "Which should be ideally used for the top heading?"
+    ),
+    React.createElement("h2", {}, "The h1 tag is preferred. "),
   ]),
 ]);
 
